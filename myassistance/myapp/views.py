e833ed3a1461b7e6d04bed137b514c74657d185f
Vaseem <vazeemkummali@gmail.com>
2025-07-08 17:12:47 +0530

    updated brain

--- myassistance/myapp/views.py
+++ myassistance/myapp/views.py
@@ -279,11 +279,11 @@ def chat_view(request):
         elif "what is the date today" in message or "today's date" in message or "date" in message or "today date" in message or "today date?" in message:
             reply = "Today's date is " + str(datetime.date.today()) + " 📅"
 
-        elif "what is the time now" in message or "current time" in message or "time" in message:
+        elif any(kw in message.lower() for kw in ["what is the time now", "current time", "time"]):
             now = datetime.datetime.now().strftime("%I:%M %p")
             reply = "The current time is " + now + " ⏰"
 
-        elif "what is your power" in message or "your powers" in message or "what can you do" in message or "what power you have" in message:
+        elif "what is your power" in message or "your powers" in message or "what can you do" in message or "what power you have" in message or "what is your capacity" in message:
             reply = "My current power is to chat, reply smartly, and assist with basic tasks 💬. I may be simple now, but with Vazeem’s updates, I’ll get more powerful day by day 💪⚡"
 
         elif "who are you" in message:
@@ -303,8 +303,17 @@ def chat_view(request):
             reply = "Not yet, but I can learn Tamil too if my developer trains me 🎓"
         elif "can you speak hindi" in message or "do you know hindi" in message:
             reply = "Not yet, but if my developer teaches me, I will learn Hindi too 🇮🇳"
+        elif "do you know how many months are included in an year" in message or "how many month in a year" in message or "months i year" in message:
+            reply = "yes, 12 months are in a year"
+        elif "why are you created" in message or "why created you" in message:
+            reply = "I was created by Vaszeem to assist you with anything you need. 😊"
+
+        elif "what can you do" in message:
+            reply = "I can answer your questions, tell the time, and much more soon!"
 
 
+
+            
         else:
             reply = "Sorry, I didn't understand that. Can you rephrase or try asking differently? 🤔"
 
